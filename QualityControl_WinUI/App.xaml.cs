fd1ea8993ce820884f9e287034521bdbf86c79e6
teperyaemo <rokudenas@bk.ru>
2023-04-30 23:31:33 +0300

    Добавьте файлы проекта.

---
+++ QualityControl_WinUI/App.xaml.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using WinRT; // required to support Window.As<ICompositionSupportsSystemBackdrop>()
+using QualityControl_WinUI.Helpers;
+using Microsoft.UI.Composition.SystemBackdrops;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace QualityControl_WinUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+
+            if (AppWindowTitleBar.IsCustomizationSupported()) //Run only on Windows 11
+            {
+                m_window.SizeChanged += SizeChanged; //Register handler for setting draggable rects
+            }
+            m_window.Activate();
+        }
+
+        private void SizeChanged(object sender, WindowSizeChangedEventArgs args)
+        {
+            //Update the title bar draggable region. We need to indent from the left both for the nav back button and to avoid the system menu
+            Windows.Graphics.RectInt32[] rects = new Windows.Graphics.RectInt32[] { new Windows.Graphics.RectInt32(48, 0, (int)args.Size.Width - 48, 48) };
+            
+        }
+
+        private Window m_window;
+    }
+}
